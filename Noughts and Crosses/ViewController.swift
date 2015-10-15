797257e5993f3ef0d8a3f3cf74fcd977985e3f96
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-15 17:52:56 -0500

    Added code for winning logic and label

--- Noughts and Crosses/ViewController.swift
+++ Noughts and Crosses/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         //0 represents no selection, 1 = nought, and 2 = cross
     
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
     @IBOutlet var button: UIButton!
+    
+    @IBOutlet var gameOverLabel: UILabel!
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
@@ -36,6 +40,33 @@ class ViewController: UIViewController {
                 activePlayer = 1
             
             }
+            
+            for combination in winningCombinations {
+                
+                if (gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]) {
+                    
+                    if gameState[combination[0]] == 1 {
+                        
+                        gameOverLabel.text = "Noughts have won!"
+                        
+                    } else {
+                        
+                        gameOverLabel.text = "Crosses have won!"
+                        
+                    }
+                    
+                    gameOverLabel.hidden = false
+                    
+                    UIView.animateWithDuration(0.5, animations: { () -> Void in
+                        
+                        self.gameOverLabel.center = CGPointMake(self.gameOverLabel.center.x + 500, self.gameOverLabel.center.y)
+                        
+                        
+                    })
+                    
+                }
+                
+            }
         }
         
     }
@@ -45,6 +76,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        gameOverLabel.hidden = true
+        
+        gameOverLabel.center = CGPointMake(gameOverLabel.center.x - 500, gameOverLabel.center.y)
+        
     }
 
     override func didReceiveMemoryWarning() {
